edab435a864a42a50ac040e18e01db843f0a843a
Glenn Streiff <gsnospampls@pobox.com>
2019-06-15 21:23:31 -0500

    bootstrap a heroku-deployed project that uses
    mysql via jawsdb
    react
    and sequelize

---
+++ README.md
@@ -0,0 +1,45 @@
+# greenleaves
+
+Greenleaves is a business-to-business (B2B) fullstack web application enabling customers to place orders with an Asian vegetable wholesaler for pickup or delivery.  Orders may be placed in the following languages:
+
+* English
+* Vietnamese
+* Korean
+* Chinese (Mandarin)
+
+Additionally, the application allows an administrator to upload and edit the product catalog.
+
+## Technology Stack
+
+### Frontend
+
+* React with Material Design UI
+* i18n with react-18next(?)
+* Javascript
+* HTML
+* CSS
+
+### Backend
+
+* RESTful API
+* Javascript
+* NodeJS
+* ExpressJS web server
+* Sequelize ORM
+* MySQL
+* Authentication with Auth0(?)
+* i18n with 18next(?)
+
+## Design
+
+### Customer Activity Diagram
+
+![alt](docs/img/uml-ad-cust.png)
+
+### Administrator Activity Diagram
+
+![alt](docs/img/uml-ad-admin.png)
+
+### Data Design (Entity Relationship Diagram)
+
+![alt](/docs/img/greenleaf_ERD.png)
---
+++ config/config.js
@@ -0,0 +1,41 @@
+function getDBConfigFromUrl(url){
+  const firstSplit = url.split(":");
+  const secondSplit= firstSplit[2].split("@");
+
+  return {
+    "username": firstSplit[1].split("//").slice(-1)[0],
+    "password": secondSplit[0],
+    "database": firstSplit[3].split("/").slice(-1)[0],
+    "host": secondSplit[1],
+  };
+
+}
+
+const config = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "local_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+};
+
+if(process.env.NODE_ENV === "production"){
+  config.production = {
+    ...getDBConfigFromUrl(process.env.JAWSDB_URL),
+    "dialect": "mysql",
+    "operatorsAliases": false
+  };
+}
+
+module.exports = config;
