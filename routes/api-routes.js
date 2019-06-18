6644bcd36f7b21e997f6aab36827140630bf8121
tuequach <tuequach@gmail.com>
2019-06-17 20:01:44 -0500

    api-routes for products

---
+++ routes/api-routes.js
@@ -0,0 +1,49 @@
+/* eslint-disable indent */
+
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the products
+  app.get("/api/products", function(req, res) {
+      db.Product.findAll({}).then(function(dbProduct){
+        res.json(dbProduct);
+      });
+  });
+
+  // Get route for returning posts of a specific category
+  app.post("/api/admin/add", function(req, res) {
+      db.Product.create({
+         name: req.body.name,
+         quantity: req.body.quantity,
+         unit: req.body.unit
+      }).then(function(dbProduct){
+        res.json(dbProduct);
+      });
+    // Add sequelize code to find all posts where the category is equal to req.params.category,
+    // return the result to the user with res.json
+  });
+
+
+  // Get route for retrieving a single product by id
+  app.get("/api/products/:id", function(req, res) {
+      db.Product.findOne({
+        id: req.params.id
+      }).then(function(dbProduct){
+        res.json(dbProduct);
+      });
+   
+  });
+
+  // Get route for retrieving  all product by name
+  app.get("/api/products/:name", function(req, res) {
+    db.Product.findAll({
+      name: req.params.name
+    }).then(function(dbProduct){
+      res.json(dbProduct);
+    });
+ 
+});
+};
