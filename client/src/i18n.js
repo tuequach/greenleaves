76a24221276dc06e026c56b95c6e011a458a0292
Roy Park <donghuip90@gmail.com>
2019-06-19 17:44:42 -0500

    initialized i18n.js and linked it to index.js. App.js has not been touch at this moment.

---
+++ client/src/i18n.js
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+
+i18n
+  .use(Backend)
+  // detect user language
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+
+  //init lets 'untranslated text' default back to english
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    }
+  });
+
+
+export default i18n;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./App";
+import "./i18n";
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
